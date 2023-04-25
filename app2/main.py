96be509aad5c7cf9ce9768870bf7e4c33334d1ec
Samuel Jrz <samueljuarez_@outlook.com>
2023-04-24 18:52:31 -0600

    Curso PIP y venv finalizado

--- app2/main.py
+++ app2/main.py
@@ -1,9 +1,11 @@
 import readcsv
 import utils
 import chart
+import pandas as pd
 
 def run():
     data = readcsv.leercsv('data.csv')
+    df = pd.read_csv('data.csv')
     lables, values, pais = utils.filtro(data)
     chart.graficarbar(lables, values, pais)
     chart.graficarpie(lables, values, pais)
--- app2/utils.py
+++ app2/utils.py
@@ -2,12 +2,19 @@ def filtro(path):
     pais = str
     pais = str(input('Escribe el pais a graficar: '))
     pais = pais.title()
+    '''
+    df = path[path['Country/Territory'] == pais]
+    df = df[df['Population'] in df]
+    print(df)
+
+    '''
     countrydict= filter(lambda p: p['Country/Territory'] == pais, path)
     population = {}
     for i in countrydict:
         for k,v in i.items():
             if 'Population' in k and not 'World' in k:
                 population [k[:4]] = int(v)
+
     lables = population.keys()
     values = population.values()
     return lables, values, pais
